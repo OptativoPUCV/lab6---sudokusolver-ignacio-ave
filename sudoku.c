b8e1f5693e6a95e033a9676795ba96127a0bc51d
Your Name <you@example.com>
2023-05-02 01:30:01 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -167,7 +167,7 @@ Node * DFS(Node* initial, int* cont){
     }
     return NULL;
 }
-/*
+
 
 
 int main( int argc, char *argv[] ){
@@ -182,4 +182,4 @@ int main( int argc, char *argv[] ){
   return 0;
 }
 
-*/
+
